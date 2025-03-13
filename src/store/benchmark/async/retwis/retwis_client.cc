2bc0d8bb04629877eb8e7b95b3a474d9d28b3289
Jennifer Lam <jl87@princeton.edu>
2025-03-13 01:19:30 -0400

    95/5 read write, 450us window

--- src/store/benchmark/async/retwis/retwis_client.cc
+++ src/store/benchmark/async/retwis/retwis_client.cc
@@ -31,16 +31,16 @@ namespace retwis {
     }
 
     AsyncTransaction *RetwisClient::GetNextTransaction() {
-        lastOp = "one_shot_rw";
-        return new OneShotRW(keySelector, GetRand());
-        // int ttype = GetRand()() % 100;
-        // if (ttype < 50) {
-        // lastOp = "one_shot_writes";
-        // return new OneShotWrites(keySelector, GetRand());
-        // } else {
-        // lastOp = "one_shot_reads";
-        // return new OneShotReads(keySelector, GetRand());
-        // }
+//        lastOp = "one_shot_rw";
+//        return new OneShotRW(keySelector, GetRand());
+        int ttype = GetRand()() % 100;
+        if (ttype < 5) {
+            lastOp = "one_shot_writes";
+            return new OneShotWrites(keySelector, GetRand());
+        } else {
+            lastOp = "one_shot_reads";
+            return new OneShotReads(keySelector, GetRand());
+        }
     }
 
 }  //namespace retwis
