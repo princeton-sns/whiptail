6b6a59898ca4633dbcf4820187a072869123014c
Jennifer Lam <jl87@princeton.edu>
2025-03-16 15:47:44 -0400

    Change OneShotReads to use BeginRW and Get

--- src/store/benchmark/async/retwis/one_shot_reads.cc
+++ src/store/benchmark/async/retwis/one_shot_reads.cc
@@ -14,13 +14,11 @@ namespace retwis {
     Operation OneShotReads::GetNextOperation(std::size_t op_index) {
         Debug("ONE_SHOT_READS %lu", op_index);
         if (op_index == 0) {
-            return BeginRO();
+            return BeginRW();
         } else if (op_index == 1) {
-            std::unordered_set<std::string> keys;
-            for (std::size_t i = 0; i < 1; i++) {
-                keys.insert(GetKey(i));
-            }
-            return ROCommit(keys);
+            return Get(GetKey(0));
+        } else if (op_index == 2) {
+            return Commit();
         } else {
             return Wait();
         }
