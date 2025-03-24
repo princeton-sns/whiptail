4b9826d52545db1881f9ba8543feae9a2d620601
Jennifer Lam <jl87@princeton.edu>
2025-03-24 12:24:19 -0400

    more notice statements

--- src/lib/tcptransport.cc
+++ src/lib/tcptransport.cc
@@ -425,7 +425,7 @@ void TCPTransport::Register(TransportReceiver *receiver,
     receivers[fd] = receiver;
     fds[receiver] = fd;
 
-    Debug("Accepting connections on TCP port %hu", ntohs(sin.sin_port));
+    Notice("Accepting connections on TCP port %hu", ntohs(sin.sin_port));
 }
 
 bool TCPTransport::SendMessageInternal(TransportReceiver *src,
