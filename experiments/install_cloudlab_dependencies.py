1a72295445e0196a2f5920fe64a9efe1471bce6f
Jennifer Lam <jl87@princeton.edu>
2025-04-12 00:28:17 -0400

    Add perf

--- experiments/install_cloudlab_dependencies.py
+++ experiments/install_cloudlab_dependencies.py
@@ -46,6 +46,31 @@ def setup_aliases():
         "echo \"alias r=\'cd /mnt/extra/experiments\'\" >> /root/.bash_aliases")
 
 
+def setup_perf():
+    call("apt install -y linux-tools-4.15.0-151-generic linux-cloud-tools-4.15.0-151-generic linux-tools-generic")
+
+    hosts = [
+        "us-east-1-0",
+        "us-east-1-1",
+        "us-east-1-2",
+        "us-west-1-0",
+        "us-west-1-1",
+        "us-west-1-2",
+        "eu-west-1-0",
+        "eu-west-1-1",
+        "eu-west-1-2",
+        "client-0-0",
+        "client-0-1",
+        "client-1-0",
+        "client-1-1",
+        "client-2-0",
+        "client-2-1"]
+
+    for host in hosts:
+        call_remote(host,
+                    "apt update && apt install -y linux-tools-4.15.0-151-generic linux-cloud-tools-4.15.0-151-generic linux-tools-generic")
+
+
 def main():
     call("apt update")
     call("apt install gnuplot -y")
